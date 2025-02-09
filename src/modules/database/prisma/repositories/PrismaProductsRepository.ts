b4cac1569a990be0a31b19f8bdf9fe505a6f3f16
Vinicius Costa <vinicius.dsc95@gmail.com>
2025-02-09 16:38:19 -0300

    chore: add prisma migration

--- src/modules/database/prisma/repositories/PrismaProductsRepository.ts
+++ src/modules/database/prisma/repositories/PrismaProductsRepository.ts
@@ -5,11 +5,17 @@ import { Product } from '@prisma/client';
 import { GetAllProductsDto } from 'src/modules/products/dtos/getAllProductsDto';
 import { UpdateProductDto } from 'src/modules/products/dtos/updateProductDto';
 
+const DEFAULT_PAGE = 0;
+const DEFAULT_PAGE_SIZE = 20;
+
 @Injectable()
 export class PrismaProductsRepository implements ProductsRepository {
   constructor(private readonly prismaService: PrismaService) {}
 
-  getAll({ page = 0, size = 20 }: GetAllProductsDto): Promise<Product[]> {
+  getAll(getAllProductsDto: GetAllProductsDto): Promise<Product[]> {
+    const page = getAllProductsDto.page || DEFAULT_PAGE;
+    const size = getAllProductsDto.size || DEFAULT_PAGE_SIZE;
+
     return this.prismaService.product.findMany({
       take: size,
       skip: page * size,
@@ -22,8 +28,8 @@ export class PrismaProductsRepository implements ProductsRepository {
     });
   }
 
-  create(product: Product): Promise<Product> {
-    return this.prismaService.product.create({ data: product });
+  async create(product: Product): Promise<Product> {
+    return await this.prismaService.product.create({ data: product });
   }
 
   update(id: Product['id'], data: UpdateProductDto): Promise<Product> {
